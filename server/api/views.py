edf3b24d1e0e24b6a782594eba6e5536ccbf700d
Andy Ansong <andyansong82@gmail.com>
2026-02-01 00:37:07 +0000

    setup backend project

--- server/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def getExample(request):
-    example = {'name': 'John', 'age': 50}
-    return Response(example)
