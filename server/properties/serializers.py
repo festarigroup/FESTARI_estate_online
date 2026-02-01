ddc3d1306b55526761148bc52a9409d8903f19ad
Andy Ansong <andyansong82@gmail.com>
2026-02-01 01:55:31 +0000

    wishlist endpoint

--- server/properties/serializers.py
+++ server/properties/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Property, PropertyImage, Wishlist
+from .models import Property, PropertyImage
 
 
 class PropertyImageSerializer(serializers.ModelSerializer):
