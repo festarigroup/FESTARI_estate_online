179c28cb9f794f48fd56408b8b6b2169abc3c2a0
Andy Ansong <andyansong82@gmail.com>
2026-02-07 22:40:55 +0000

    attach html to email

--- server/authentication/utils.py
+++ server/authentication/utils.py
@@ -1,12 +1,22 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 
 def send_otp_email(email, code):
-    send_mail(
-        subject="Your Festari Verification Code",
-        message=f"Your OTP is {code}. It expires in 10 minutes.",
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[email],
-        fail_silently=False,
+    html_content = render_to_string(
+        "templates/emails/my_email.html",
+        context={"OTP": code},
     )
+
+    msg = EmailMultiAlternatives(
+        subject="Festari Estate - Activate account",
+        body="Activate your account using the OTP provided.",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[email],
+        headers={"List-Unsubscribe": "<mailto:unsub@example.com>"},
+    )
+
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
