5b84cb40e59fd20064c2053de51d3efeb3ef67e4
Andy Ansong <andyansong82@gmail.com>
2026-02-12 00:56:54 +0000

    implement rata limiting

--- server/festari_estate/settings.py
+++ server/festari_estate/settings.py
@@ -159,6 +159,12 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'EXCEPTION_HANDLER': 'utils.exceptions.custom_exception_handler',
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "user": "200/hour",
+    }
 }
 
 SIMPLE_JWT = {
