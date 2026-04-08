8e4461365554dd835ce8067242ee838cc651adf2
Grld-Andy <andyansong82@gmail.com>
2026-04-08 11:15:47 +0000

    feat: complete auth endpoints

---
+++ client/src/services/auth/request-password-reset.ts
@@ -0,0 +1,16 @@
+import { publicApi } from "../axios-wrapper"
+
+export interface RequestPasswordResetType{
+    email: string
+}
+
+export const RequestPasswordReset = async (data: RequestPasswordResetType) => {
+    try{
+        const response = await publicApi.post("/auth/request_password_reset", {
+            ...data
+        })
+        console.log(response)
+    }catch(err){
+        console.log(err)
+    }
+}
